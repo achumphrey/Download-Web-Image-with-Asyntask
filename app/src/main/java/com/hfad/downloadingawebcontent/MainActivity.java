59e4c1067cf154fd006ad281a89755bea7e3e302
AcHumphrey <AcHumphrey@example.com>
2019-08-05 22:48:14 +0100

    Initial commit

---
+++ app/src/main/java/com/hfad/downloadingawebcontent/MainActivity.java
@@ -0,0 +1,71 @@
+package com.hfad.downloadingawebcontent;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    public void downloadImage(View view){
+
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myBitmap;
+
+        try {
+          myBitmap =  task.execute("https://vignette.wikia.nocookie.net/sonic-and-friends/images/5/5d/Homer_Simpson.jpg/revision/latest?cb=20180218220951").get();
+          imageView.setImageBitmap(myBitmap);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+
+                //get a url connection in order to connect to any http address
+                URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+
+                //get the input stream reader to read from the site
+                InputStream inputStream = urlConnection.getInputStream();
+
+                //create a bitmap decoder to translate the input stream into bitmap data type
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+
+        }
+    }
+}
